c109c4599f85a2a855d1062e2adb758edda0ef18
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-10-23 11:18:24 +0200

    added style.css and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const dialogRef = document.getElementById('myDialog');
+
+        function openDialog() {
+            dialogRef.showModal();
+        }
+        function closeDialog() {
+            dialogRef.close();
+        }
