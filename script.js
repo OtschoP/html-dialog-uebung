834cdd26707a2ee99b6c2fa3cba1f0618023381a
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-10-23 11:32:54 +0200

    added transform to css

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const dialogRef = document.getElementById('myDialog');
 
         function openDialog() {
             dialogRef.showModal();
+            dialogRef.classList.add('active');
         }
         function closeDialog() {
             dialogRef.close();
+            dialogRef.classList.remove('active');
         }
